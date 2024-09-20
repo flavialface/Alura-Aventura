e23a2fff6e8c647d91050e510c1f0c54cd47ae09
Flavisocafofo <00001101803976sp@al.educacao.sp.gov.br>
2024-09-20 20:44:23 -0300

    Update inscript.js

--- inscript.js
+++ inscript.js
@@ -1,11 +1,1 @@
-const avanca = documento.querySelectorA11('.btn-proximo');
 
-avanca.forEach(button => { 
-    button.addEventiListener('clik', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
